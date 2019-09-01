c7ae9cd3c298f8eb2557bcde63b15598213aa6c2
GeekZJJ <geekzjj@gmail.com>
2019-09-01 21:37:03 +0000

    add build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#export PATH="/usr/lib/ccache:$PATH"
+export USE_CCACHE=1
+
+#export PATH=$PATH:/root/aarch64-linux-android-4.9/bin
+export CROSS_COMPILE="ccache aarch64-linux-android-"
+
+export ARCH=arm64
+export SRCARCH=arm64
+export SUBARCH=arm64
+
+mkdir -p out && make clean O=out && make mrproper O=out && make msm-perf_defconfig O=out && time make -j5 O=out
+mkdir -p release/
+rm release/*
+find -name "*.ko" | xargs -i cp {} release/
+cp out/arch/arm64/boot/Image.gz-dtb release/
